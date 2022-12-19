c242a1075b0771d1176714fda581d9b599be5f95
KangBob <brespiar@gmail.com>
2022-12-19 19:33:38 +0700

    labweb11

---
+++ latihan1.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2022 at 01:24 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.0.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latihan1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `id_barang` int(10) NOT NULL,
+  `kategori` varchar(30) DEFAULT NULL,
+  `nama` varchar(30) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  `harga_beli` decimal(10,0) DEFAULT NULL,
+  `harga_jual` decimal(10,0) DEFAULT NULL,
+  `stok` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_barang`
+--
+
+INSERT INTO `data_barang` (`id_barang`, `kategori`, `nama`, `gambar`, `harga_beli`, `harga_jual`, `stok`) VALUES
+(2, 'Elektronik', 'promekk', 'hp_xiaomi.jpg', '10000000', '8000000', 5),
+(3, 'Elektronik', 'HP OPPO Android', 'hp_oppo.jpg', '1800000', '2300000', 5),
+(4, 'Komputer', 'Legion', '', '12000000', '15000000', 2),
+(5, 'Hand Phone', 'HP SAMSUNG', '', '2400000', '2000000', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', '0cd60efb5578cd967c3c23894f305800');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQUE` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
